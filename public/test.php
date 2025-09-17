3b3750f5899e389906dd4376c9557403c603306e
doanhhq <hoangquocdoanh6686@gmail.com>
2025-09-17 22:25:32 +0700

    new update

--- public/test.php
+++
@@ -1,7 +0,0 @@
-<?php
-echo "PHP Version: " . phpversion() . "<br>";
-echo "Laravel FitZone is working!<br>";
-echo "Assets built successfully!<br>";
-echo "<a href='/login'>Go to Login</a><br>";
-echo "<a href='/register'>Go to Register</a>";
-?>
